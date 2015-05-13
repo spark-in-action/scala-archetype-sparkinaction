3091f7fc4a76021c00caa957e39b801be42e3baf
mbonaci <mbonaci@gmail.com>
2015-05-13 22:05:58 +0200

    Remove user variable (v0.9)

--- src/main/resources/archetype-resources/src/main/scala/App.scala
+++ src/main/resources/archetype-resources/src/main/scala/App.scala
@@ -4,9 +4,7 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 
-/**
- * @author ${user.name}
- */
+
 object App {
 
   def main(args : Array[String]) {
